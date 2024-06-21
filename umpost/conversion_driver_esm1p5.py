efd1698b4be4f05e9412cce505885f0fb6533988
Spencer Wong <spencer.wong@anu.edu.au>
2024-06-21 15:42:17 +1000

    Add conversion driver for use during esm1p5 simulations. Initial version uses um2netcdf4
    Rebase to include new umpost directory

---
+++ umpost/conversion_driver_esm1p5.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+# First attempt at running um to netcdf conversion as a userscript for
+# ESM1.5. Adapted from Martin Dix's conversion driver for CM2
+
+
+import os
+import collections
+import um2netcdf4
+import re
+import f90nml
+import warnings
+import argparse
+import errno
+from pathlib import Path
+from collections.abc import Sequence
+
+
+def convert_esm1p5_dir(FF_dir, NC_dir, FF_name_pattern):
+    """Convert all fields file ESM1.5 UM outputs in FF_dir to netcdf format in NC_dir"""
+
+    if isinstance(FF_dir, str):
+        FF_dir = Path(FF_dir)
+
+    # Find output fields files in the specified directory
+    output_fields_files = [
+        filename
+        for filename in os.listdir(FF_dir)
+        if re.match(FF_name_pattern, filename)
+    ]
+
+    for FF_name in output_fields_files:
+        FF_file_path = FF_dir / FF_name
+        NC_name = FF_name + ".nc"
+        NC_file_path = NC_dir / NC_name
+
+        # Named tuple to hold the argument list
+        Args = collections.namedtuple(
+            "Args",
+            "nckind compression simple nomask hcrit verbose include_list exclude_list nohist use64bit",
+        )
+        args = Args(3, 4, True, False, 0.5, False, None, None, False, False)
+
+        print("Converting file " + FF_name)
+
+        try:
+            um2netcdf4.process(FF_file_path, NC_file_path, args)
+
+        except Exception as exc:
+            # Not ideal here - um2netcdf4 raises generic exception when missing coordinates
+            if exc.args[0] == "Variable can not be processed":
+                warnings.warn("Unable to convert file: " + FF_name)
+                pass
+            else:
+                raise
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "current_output_dir", help="ESM1.5 output directory to be converted", type=str
+    )
+    args = parser.parse_args()
+
+    current_run_output_dir = Path(args.current_output_dir)
+    current_run_FF_dir = current_run_output_dir / "atmosphere"
+
+    # Check that the directory containing fields files for conversion exists
+    if not (current_run_FF_dir.is_dir()):
+        raise FileNotFoundError(
+            errno.ENOENT, os.strerror(errno.ENOENT), current_run_FF_dir
+        )
+
+    current_run_NC_dir = current_run_FF_dir / "netCDF"
+    current_run_NC_dir.mkdir(exist_ok=True)
+
+    # Find the run_id used in the file names
+    xhist_nml = f90nml.read(current_run_FF_dir / "xhist")
+    run_id = xhist_nml["nlchisto"]["run_id"]
+
+    # For ESM1.5 simulations, files start with run_id + 'a' (atmosphere) +
+    # '.' (absolute standard time convention) + 'p' (pp file).
+    # See get_name.F90 in the UM7.3 source code for details.
+    FF_name_pattern = rf"^{run_id}a.p[a-z0-9]+$"
+
+    convert_esm1p5_dir(current_run_FF_dir, current_run_NC_dir, FF_name_pattern)
