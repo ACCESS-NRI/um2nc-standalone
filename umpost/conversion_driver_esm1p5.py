667843b6e527d6c2f456e807d2eac13aadebc64d
Spencer Wong <spencer.wong@anu.edu.au>
2024-06-26 14:56:44 +1000

    Split convert_esm1p5_dir function into smaller pieces

--- umpost/conversion_driver_esm1p5.py
+++ umpost/conversion_driver_esm1p5.py
@@ -22,35 +22,67 @@ import errno
 from pathlib import Path
 
 
-def convert_esm1p5_dir(ff_dir, nc_dir, FF_name_pattern):
+def list_ff_outputs(ff_dir, ff_name_pattern):
     """
-    Convert ESM1.5 fields file outputs in specified directory to netCDF.
-
-    Parameters
-    ----------
-    ff_dir : Path to source directory containing UM fields files for conversion.
-    nc_dir : Path to target directory for saving NetCDF files.
-    FF_naming_pattern : Regex pattern. Files with matching names will be converted.
-
-    Returns
-    -------
-    None
+    List paths to files in ff_dir with names matching ff_name_pattern.
     """
 
     if isinstance(ff_dir, str):
         ff_dir = Path(ff_dir)
 
     # Find output fields files in the specified directory
-    output_fields_files = [
-        filename
-        for filename in os.listdir(ff_dir)
-        if re.match(FF_name_pattern, filename)
-    ]
+    ff_paths = []
+    for filepath in ff_dir.glob("*"):
+        if re.match(ff_name_pattern, filepath.name):
+            ff_paths.append(filepath)
 
-    for FF_name in output_fields_files:
-        FF_file_path = ff_dir / FF_name
-        NC_name = FF_name + ".nc"
-        NC_file_path = nc_dir / NC_name
+    return ff_paths 
+    
+
+def set_nc_path(ff_path, nc_dir):
+    """
+    Create path to write converted NetCDF file to
+
+    Parameters
+    ----------
+    ff_path : path to UM fields file to be converted.
+    nc_dir : path to target directory for saving NetCDF files.
+
+    Returns
+    -------
+    nc_path : path for writing converted file.
+    """
+    ff_path = Path(ff_path) if isinstance(ff_path, str) else ff_path
+
+    ff_name = ff_path.name 
+    nc_name = ff_name + ".nc"
+    nc_path = nc_dir / nc_name 
+
+    return nc_path
+
+
+
+
+
+def convert_ff_list(ff_path_list, nc_dir):
+    """
+    Convert listed fields files to netCDF.
+
+    Parameters
+    ----------
+    ff_path_list : paths to fields files to be converted.
+    nc_dir : path to target directory for saving NetCDF files.
+
+    Returns
+    -------
+    None
+    """
+
+
+    for ff_path in ff_path_list:
+
+        ff_path = Path(ff_path) if isinstance(ff_path, str) else ff_path
+        nc_path = set_nc_path(ff_path, nc_dir)   
 
         # TODO: um2netcdf will update the way arguments are fed to `process`.
         # https://github.com/ACCESS-NRI/um2nc-standalone/issues/17
@@ -64,19 +96,24 @@ def convert_esm1p5_dir(ff_dir, nc_dir, FF_name_pattern):
         # TODO: Confirm with Martin the below arguments are appropriate defaults.
         args = Args(3, 4, True, False, 0.5, False, None, None, False, False)
 
-        print("Converting file " + FF_name)
+        print("Converting file " + ff_path.name)
 
         try:
-            um2netcdf4.process(FF_file_path, NC_file_path, args)
+            um2netcdf4.process(ff_path, nc_path, args)
 
         except Exception as exc:
             # Not ideal here - um2netcdf4 raises generic exception when missing coordinates
             if exc.args[0] == "Variable can not be processed":
-                warnings.warn("Unable to convert file: " + FF_name)
+                warnings.warn("Unable to convert file: " + ff_path.name)
             else:
                 raise
 
 
+
+
+
+
+    
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -104,6 +141,8 @@ if __name__ == "__main__":
     # For ESM1.5 simulations, files start with run_id + 'a' (atmosphere) +
     # '.' (absolute standard time convention) + 'p' (pp file).
     # See get_name.F90 in the UM7.3 source code for details.
-    FF_name_pattern = rf"^{run_id}a.p[a-z0-9]+$"
+    ff_name_pattern = rf"^{run_id}a.p[a-z0-9]+$"
 
-    convert_esm1p5_dir(current_run_ff_dir, current_run_nc_dir, FF_name_pattern)
+    current_run_ff_outputs = list_ff_outputs(current_run_ff_dir, ff_name_pattern)
+
+    convert_ff_list(current_run_ff_outputs, current_run_nc_dir)
