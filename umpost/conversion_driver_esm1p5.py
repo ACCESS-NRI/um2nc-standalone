3d9fbcd0c602e8ff85d1fe320ea098b5f2b30b61
Spencer Wong <spencer.wong@anu.edu.au>
2024-08-21 11:40:03 +1000

    Split main into separate function and add entry point

--- umpost/conversion_driver_esm1p5.py
+++ umpost/conversion_driver_esm1p5.py
@@ -12,6 +12,7 @@ https://github.com/ACCESS-NRI/access-cm2-drivers/blob/main/src/run_um2netcdf.py
 
 
 import os
+import sys
 import collections
 import re
 import f90nml
@@ -295,25 +296,36 @@ def safe_removal(succeeded, failed):
     return succeeded_inputs - failed_inputs
 
 
-if __name__ == "__main__":
+def parse_args(args):
+    """
+    Parse arguments given as list (args)
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "current_output_dir", help="ESM1.5 output directory to be converted", type=str
+        "current_output_dir", help="ESM1.5 output directory to be converted",
+        type=str
     )
     parser.add_argument("--quiet", "-q", action="store_true",
                         help=(
-                            "Report only final exception type and message for allowed"
-                            "exceptions raised during conversion when flag is included."
-                            "Otherwise report full stack trace."
+                            "Report only final exception type and message for "
+                            "allowed exceptions raised during conversion when "
+                            "flag is included. Otherwise report full "
+                            "stack trace."
                         )
                         )
     parser.add_argument("--delete-ff", "-d", action="store_true",
                         help="Delete fields files upon successful conversion."
                         )
-    args = parser.parse_args()
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args(sys.argv[1:])
 
     current_output_dir = args.current_output_dir
 
+    # Run conversion
     successes, failures = convert_esm1p5_output_dir(current_output_dir)
 
     # Report results to user
@@ -326,3 +338,8 @@ if __name__ == "__main__":
         # Remove files that appear only as successful conversions
         for path in safe_removal(successes, failures):
             os.remove(path)
+
+
+if __name__ == "__main__":
+
+    main()
