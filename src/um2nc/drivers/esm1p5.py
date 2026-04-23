0522a476beb0e96512d2fa2eec097541c87c2112
Spencer Wong <88933912+blimlim@users.noreply.github.com>
2026-04-23 15:34:03 +1000

    Introduce ModelDriver class and esm1.6 driver (#229)
    * Add abstract ModelDriver class and implement Esm1p5Driver and Esm1p6Driver as subclasses
    
    * Add tests of new model drivers
    
    * Update Esm1p6 output filenames to match schema
    
    ---------
    
    Co-authored-by: Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
    Co-authored-by: Davide Marchegiani <davide.marchegiani@gmail.com>

--- src/um2nc/drivers/esm1p5.py
+++ src/um2nc/drivers/esm1p5.py
@@ -1,161 +1,139 @@
-#!/usr/bin/env python3
 """
 ESM1.5 conversion driver
 
-Wrapper script for automated fields file to netCDF conversion
-during ESM1.5 simulations. Runs conversion module
-on each atmospheric output in a specified directory.
+Defines a ModelDriver class for running the conversion on ESM1.5 history
+directories and ESM specific functions used during the conversion.
 
 Adapted from Martin Dix's conversion driver for CM2:
 https://github.com/ACCESS-NRI/access-cm2-drivers/blob/main/src/run_um2netcdf.py
 """
 
-
-import os
-import f90nml
-import warnings
 import errno
+import os
+import re
+import warnings
 
-from pathlib import Path
+import f90nml
+
+from functools import cached_property
 
 from um2nc.drivers.common import find_matching_files, get_ff_date
-from um2nc.drivers.common import filter_name_collisions, safe_removal
-from um2nc.drivers.common import get_fields_file_pattern
-from um2nc.drivers.common import convert_fields_file_list
+from um2nc.drivers.common import ModelDriver
+from um2nc.um2netcdf import process
 
 
 # Character in filenames specifying the unit key
 FF_UNIT_INDEX = 8
-# Output file suffix for each type of unit. Assume's
-# ESM1.5's unit definitions are being used.
-FF_UNIT_SUFFIX = {
-    "a": "mon",
-    "e": "dai",
-    "i": "3hr",
-    "j": "6hr",
-}
+
+# Output file suffix for each type of unit.
+ESM1P5_UNIT_SUFFIXES = {
+        "a": "mon",
+        "e": "dai",
+        "i": "3hr",
+        "j": "6hr",
+    }
 
 
-def get_nc_write_path(fields_file_path, nc_write_dir, date=None):
-    """
-    Get filepath for writing netCDF to based on fields file name and date.
+class Esm1p5Driver(ModelDriver):
 
-    Parameters
-    ----------
-    fields_file_path : path to single UM fields file to be converted.
-    nc_write_dir : path to target directory for writing netCDF files.
-    date : tuple of form (year, month, day) associated with fields file data.
+    def __init__(self, model_directory):
+        super().__init__(model_directory)
+        self._atmosphere_dir = model_directory / "atmosphere"
+        self._output_dir = self.atmosphere_dir / "netCDF"
+        self._unit_suffixes = ESM1P5_UNIT_SUFFIXES
 
-    Returns
-    -------
-    nc_write_path : path for writing converted fields_file_path file.
-    """
-    fields_file_path = Path(fields_file_path)
-    nc_write_dir = Path(nc_write_dir)
+    @property
+    def atmosphere_dir(self):
+        if not self._atmosphere_dir.exists():
+            raise FileNotFoundError(
+                errno.ENOENT, os.strerror(errno.ENOENT), self._atmosphere_dir
+            )
+        return self._atmosphere_dir
 
-    nc_file_name = get_nc_filename(fields_file_path.name, date)
+    @property
+    def output_dir(self):
+        self._output_dir.mkdir(exist_ok=True)
+        return self._output_dir
 
-    return nc_write_dir / nc_file_name
+    @property
+    def unit_suffixes(self):
+        return self._unit_suffixes
 
+    @cached_property
+    def runid(self):
+        # run ID used in input file names
+        xhist_nml = f90nml.read(self.atmosphere_dir / "xhist")
+        return xhist_nml["nlchisto"]["run_id"]
 
-def get_nc_filename(fields_file_name, date=None):
-    """
-    Format a netCDF output filename based on the input fields file name and
-    its date. Assumes fields_file_name follows ESM1.5's naming convention
-    '{5 char run_id}.pa{unit}{date encoding}`.
+    @cached_property
+    def input_name_pattern(self):
+        # regex pattern for matching input files
+        return re.compile(rf"^(?P<stem>{self.runid}a.p(?P<unit>[a-z]))[a-z0-9]+$")
 
-    Parameters
-    ----------
-    fields_file_name: name of fields file to be converted.
-    date: tuple of form (year, month, day) associated with fields file data,
-          or None. If None, ".nc" will be concatenated to the original fields
-          file name.
+    def convert(self, input_path, output_path, process_args):
+        """
+        Convert an individual input fields file to netCDF.
 
-    Returns
-    -------
-    name: formated netCDF filename for writing output.
-    """
-    if date is None:
-        return f"{fields_file_name}.nc"
+        Parameters:
+        -----------
+        input_path: Path to input fields file.
+        output_path: Path for writing output netCDF.
+        process_args: conversion arguments.
+        """
+        process(input_path, output_path, process_args)
 
-    # TODO: Use regex to extract stem and unit from filename to improve 
-    # clarity, and for better handling of unexpected filenames.
-    stem = fields_file_name[0:FF_UNIT_INDEX + 1]
-    unit = fields_file_name[FF_UNIT_INDEX]
+    def get_input_paths(self):
+        """
+        Find atmosphere fields files for conversion.
 
-    try:
-        suffix = f"_{FF_UNIT_SUFFIX[unit]}"
-    except KeyError:
-        warnings.warn(
-            f"Unit code '{unit}' from filename f{fields_file_name} "
-            "not recognized. Frequency information will not be added "
-            "to the netCDF filename.", RuntimeWarning
-        )
-        suffix = ""
+        Returns:
+        --------
+        input_paths: List of paths to UM fields files to be converted.
+        """
+        input_paths = find_matching_files(self.atmosphere_dir, self.input_name_pattern)
 
-    year, month, _ = date
-    return f"{stem}-{year:04d}{month:02d}{suffix}.nc"
+        if len(input_paths) == 0:
+            warnings.warn(
+                f"No files matching pattern '{self.input_name_pattern}' "
+                f"found in {self.atmosphere_dir.resolve()}. No files will be "
+                "converted to netCDF."
+            )
 
+        return input_paths
 
-def convert_esm1p5_output_dir(esm1p5_output_dir, process_args):
-    """
-    Driver function for converting ESM1.5 atmospheric outputs during a simulation.
+    def get_output_path(self, input_path):
+        """
+        Return the output path for a given input path.
 
-    Parameters
-    ----------
-    esm1p5_output_dir: an "outputXYZ" directory produced by an ESM1.5 simulation.
-            Fields files in the "atmosphere" subdirectory will be
-            converted to netCDF.
-    process_args: argparse Namespace object carrying processing arguments.
+        Parameters:
+        -----------
+        input_path: Path to input fields file.
 
-    Returns
-    -------
-    succeeded: list of tuples of (input, output) filepaths for successful
-               conversions.
-    failed: list of tuples of form (filepath, exception) for files which failed
-            to convert due to an allowed exception.
-    """
+        Returns:
+        --------
+        output_path: Path for writing output netCDF.
+        """
 
-    esm1p5_output_dir = Path(esm1p5_output_dir)
+        input_name = input_path.name
+        if not (match := self.input_name_pattern.match(input_name)):
+            warnings.warn(
+                f"Input filename {input_name} does not match pattern {self.input_name_pattern.pattern}."
+                "Frequency and date information will not be added to the netCDF filename.",
+                RuntimeWarning
+            )
+            return self.output_dir / f"{input_name}.nc"
 
-    current_atm_output_dir = esm1p5_output_dir / "atmosphere"
+        stem = match.group("stem")
+        unit = match.group("unit")
+        try:
+            suffix = f"_{self.unit_suffixes[unit]}"
+        except KeyError:
+            warnings.warn(
+                f"Unit code '{unit}' in filename {input_name} "
+                "not recognized. Frequency information will not be added "
+                "to the netCDF filename.", RuntimeWarning
+            )
+            suffix = ""
 
-    if not current_atm_output_dir.exists():
-        raise FileNotFoundError(
-            errno.ENOENT, os.strerror(errno.ENOENT), current_atm_output_dir
-        )
-
-    # Create a directory for writing netCDF files
-    nc_write_dir = current_atm_output_dir / "netCDF"
-    nc_write_dir.mkdir(exist_ok=True)
-
-    # Find fields file outputs to be converted
-    xhist_nml = f90nml.read(current_atm_output_dir / "xhist")
-    run_id = xhist_nml["nlchisto"]["run_id"]
-    fields_file_name_pattern = get_fields_file_pattern(run_id)
-
-    atm_dir_contents = current_atm_output_dir.glob("*")
-
-    atm_dir_fields_files = find_matching_files(
-        atm_dir_contents, fields_file_name_pattern
-    )
-
-    if len(atm_dir_fields_files) == 0:
-        warnings.warn(
-            f"No files matching pattern '{fields_file_name_pattern}' "
-            f"found in {current_atm_output_dir.resolve()}. No files will be "
-            "converted to netCDF."
-        )
-
-        return [], []  # Don't try to run the conversion
-
-    output_paths = [get_nc_write_path(path, nc_write_dir, get_ff_date(path)) for path in atm_dir_fields_files]
-    input_output_pairs = zip(atm_dir_fields_files, output_paths)
-    input_output_pairs = filter_name_collisions(input_output_pairs)
-
-    succeeded, failed = convert_fields_file_list(input_output_pairs, process_args)
-
-    if process_args.delete_ff:
-        # Remove files that appear only as successful conversions
-        for path in safe_removal(succeeded, failed):
-            os.remove(path)
+        year, month, _ = get_ff_date(input_path)
+        return self.output_dir / f"{stem}-{year:04d}{month:02d}{suffix}.nc"
