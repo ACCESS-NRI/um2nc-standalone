c10ddfbb1f27e39c3355edcfcbf572dca14bf9a0
Martin Dix <martin.dix@anu.edu.au>
2026-04-28 08:21:33 +1000

    Support older iris versions

--- src/um2nc/um2netcdf.py
+++ src/um2nc/um2netcdf.py
@@ -18,6 +18,7 @@ import warnings
 
 import cf_units
 import cftime
+import dask
 import iris.exceptions
 import iris.util
 import mule
@@ -515,7 +516,8 @@ def process(infile, outfile, args):
 
             sman.write(c, zlib=True, complevel=args.compression, unlimited_dimensions=dims, fill_value=fill)
             # Save memory by setting this to None after use
-            c.data = None
+            # c.data = None # Requires iris >= 3.12
+            c.data = dask.array.zeros(c.data.shape)
 
 def process_cubes(cubes, mv, args):
     set_item_codes(cubes)
