d2545a31236bbe53ddc06bd8deaf4bcf43d94bc8
Joshua Torrance <joshua.torrance@anu.edu.au>
2026-05-06 14:00:49 +1000

    Removed test that outputted to file

--- test/test_single_field.py
+++ test/test_single_field.py
@@ -29,54 +29,6 @@ def unpack_fieldsfile(tmp_path):
     return dst
 
 
-def runcmd(cmd, wd=None, env=None):
-    """
-    Run a command, print stderr to stdout and optionally run in working directory
-    """
-    cwd = Path.cwd() if wd is None else wd
-    local_env = os.environ.copy()
-    if env is not None:
-        local_env.update(env)
-    subprocess.run(
-        shlex.split(cmd), stderr=subprocess.STDOUT, cwd=cwd, env=local_env, check=True
-    )
-
-
-@pytest.mark.parametrize(
-    "mode",
-    [
-        "driver esm1p5",
-        "driver esm1p6",
-        "convert {input_dir}/atmosphere/aiihca.pa01apr {input_dir}/atmosphere/file.nc",
-    ]
-)
-@pytest.mark.parametrize(
-    "single_field,expected_number_nc",
-    [
-        (True, 229),
-        (False, 1),
-    ],
-)
-def test_commandline_single_field(unpack_fieldsfile, mode, single_field, expected_number_nc):
-    """
-    Test calling um2nc from the command line
-    """
-    input_dir = unpack_fieldsfile
-
-    if "driver" in mode:
-        cmd = f"um2nc {mode} {input_dir}"
-    elif "convert" in mode:
-        cmd = f"um2nc {mode.format(input_dir=input_dir)}"
-    else:
-        raise ValueError("Unrecognised mode")
-
-    cmd += " --one-nc-per-stash-variable" if single_field else ""
-
-    runcmd(cmd)
-
-    assert len(list(input_dir.glob("**/*.nc"))) == expected_number_nc
-
-
 @pytest.mark.parametrize(
     "command,driver,input,output",
     [
