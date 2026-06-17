c5a3cd8006efc4e4f6c328a8f75659168905ffcf
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2026-06-18 09:28:14 +1000

    Added DEPLOYMENT_STAGE=PRODUCTION

--- binary_dist/scripts/create_deployment_info_json.sh
+++ binary_dist/scripts/create_deployment_info_json.sh
@@ -5,7 +5,7 @@
 # - HPC_TARGET
 # - MODULE_NAME
 # - MODULE_VERSION
-# - ENV_TYPE
+# - MODULE_TYPE
 # - DEPLOYMENT_STAGE
 # - STARTED_AT
 # - MODULE_USAGE_INSTRUCTIONS
@@ -16,6 +16,11 @@
 # Usage:
 #   create_hpc_target_deployment_info_json.sh OUTPUT_JSON_FILE
 
+set -euo pipefail
+if [[ "${CONTAINERISED_ENVS_DEBUG:-0}" == "1" ]]; then
+    set -x
+fi
+
 file="$1"
 
 date=$(TZ='Australia/Sydney' date '+%FT%T %Z')
@@ -23,24 +28,24 @@ date=$(TZ='Australia/Sydney' date '+%FT%T %Z')
     --arg target "$HPC_TARGET" \
     --arg name "$MODULE_NAME" \
     --arg version "$MODULE_VERSION" \
-    --arg type "$ENV_TYPE" \
-    --arg stage "${DEPLOYMENT_STAGE:-}" \
+    --arg type "$MODULE_TYPE" \
+    --arg stage "$DEPLOYMENT_STAGE" \
     --arg started_at "$STARTED_AT" \
     --arg completed_at "$date" \
-    --arg env_usage_instructions "${MODULE_USAGE_INSTRUCTIONS:-}" \
+    --arg module_usage_instructions "${MODULE_USAGE_INSTRUCTIONS:-}" \
     --arg env_lock "${ENV_LOCK:-}" \
     --arg success "$SUCCESS" \
     '{
         "name": $target,
         "deployments": [
             {
-                "env_name": $name,
-                "env_version": $version,
-                "env_type": $type,
+                "module_name": $name,
+                "module_version": $version,
+                "module_type": $type,
                 "deployment_stage": $stage,
                 "started_at": $started_at,
                 "completed_at": $completed_at,
-                "env_usage_instructions": $env_usage_instructions,
+                "module_usage_instructions": $module_usage_instructions,
                 "env_lock": $env_lock,
                 "success": $success
             }
